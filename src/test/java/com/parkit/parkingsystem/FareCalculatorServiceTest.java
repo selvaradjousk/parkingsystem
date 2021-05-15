7821c1801ac840f1c5ec5dd29b24fc768fa90ee6
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-05-15 20:25:35 +0200

    [TEST CASE] New Test case to calculate Fare With Negative DurationTime() tested successfully

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -5,6 +5,8 @@ import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +16,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Date;
 
 /**
- * Class {@link FareCalculatorServiceTest} - Performs Unit Testing on Fare calculation process for customer of ParkIt
- * Class Tested: {@link FareCalculatorService} 
+ * Class {@link FareCalculatorServiceTest} - Performs Unit Testing on Fare
+ * calculation process for customer of ParkIt Class Tested:
+ * {@link FareCalculatorService}
  * 
  * @package - com.parkit.parkingsystem
  * @project - P3 - parking system - ParkIt
@@ -39,12 +42,13 @@ public class FareCalculatorServiceTest {
 	}
 
 	/**
-	 * {@link #calculateFareCar()} Unit Test
-	 * <br>GIVEN: One hour of parking time for a CAR
-	 * <br>WHEN: calculation of fare service activated
-	 * <br>THEN: calculation of car fare per hour
-	 * <br> <b>Test Condition <i>PASSED</i>: </b>assertEquals <code>TRUE</code> <code><b>Test</b></code> 
-	 * <br> <b>Test Condition <i>FAILED</i>: </b>assertEquals <code>FALSE</code> <code><b>Test</b></code>
+	 * {@link #calculateFareCar()} Unit Test <br>
+	 * GIVEN: One hour of parking time for a <b>CAR</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of <b>CAR</b> fare per hour <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals <code><b>TRUE</b></code>
+	 * <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals <code><b>FALSE</b></code>
 	 */
 	@Test
 	public void calculateFareCar() {
@@ -66,6 +70,16 @@ public class FareCalculatorServiceTest {
 		assertEquals(ticket.getPrice(), Fare.CAR_RATE_PER_HOUR);
 	}
 
+	/**
+	 * {@link #calculateFareRecurentUserCar()} Unit Test <br>
+	 * GIVEN: Five hour of parking time for a <b>CAR</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of <b>Recurrence of user</b> tested <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals fare = 7.125€
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals fare != 7.125€
+	 * <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareRecurentUserCar() {
 
@@ -86,6 +100,15 @@ public class FareCalculatorServiceTest {
 		assertEquals((7.125), ticket.getPrice());
 	}
 
+	/**
+	 * {@link #calculateFareCar()} Unit Test <br>
+	 * GIVEN: One hour of parking time for a <b>BIKE</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of <b>BIKE</b> fare per hour <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals <code><b>TRUE</b></code>
+	 * <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareBike() {
 
@@ -106,6 +129,16 @@ public class FareCalculatorServiceTest {
 		assertEquals(ticket.getPrice(), Fare.BIKE_RATE_PER_HOUR);
 	}
 
+	/**
+	 * {@link #calculateFareUnkownType()} Unit Test <br>
+	 * GIVEN: One hour of parking time for a <b>UNKNOWN VEHICLE TYPE</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: Identify <b>UNKNOWN VEHICLE TYPE</b> <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertThrows NullPointerException
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertThrows NullPointerException
+	 * <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareUnkownType() {
 
@@ -121,8 +154,21 @@ public class FareCalculatorServiceTest {
 
 		// THEN
 		assertThrows(NullPointerException.class, () -> fareCalculatorService.calculateFare(ticket, false));// WHEN
+		
+
 	}
 
+	/**
+	 * {@link #calculateFareBikeWithFutureInTime()} Unit Test <br>
+	 * GIVEN: Parking entry time for the <b>BIKE</b> set to <b>FUTURE TIME</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare per hour checks for entry time compliance to
+	 * current time or not <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertThrows IllegalArgumentException
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertThrows IllegalArgumentException
+	 * <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareBikeWithFutureInTime() {
 
@@ -139,7 +185,51 @@ public class FareCalculatorServiceTest {
 		// THEN
 		assertThrows(IllegalArgumentException.class, () -> fareCalculatorService.calculateFare(ticket, false)); // WHEN
 	}
+	
+	/**
+	 * {@link #calculateFareBikeWithNegativeDurationTime()} Unit Test <br>
+	 * GIVEN: Parking entry time for the <b>BIKE</b> set to <b>FUTURE TIME</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare per hour checks for entry time compliance to
+	 * current time or not <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertThrows IllegalArgumentException
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertThrows IllegalArgumentException
+	 * <code><b>FALSE</b></code>
+	 */
+	@Test
+	public void calculateFareBikeWithNegativeDurationTime() {
 
+		// GIVEN
+		Date inTime = new Date();
+//		inTime.setTime(System.currentTimeMillis() + (60 * 60 * 1000));
+//		Date outTime = new Date();
+		inTime.setTime(System.currentTimeMillis());
+		Date outTime = new Date();
+		outTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
+
+		ticket.setInTime(inTime);
+		ticket.setOutTime(outTime);
+		ticket.setParkingSpot(parkingSpot);
+
+		// THEN
+		assertThrows(IllegalArgumentException.class, () -> fareCalculatorService.calculateFare(ticket, false)); // WHEN
+	}
+
+	/**
+	 * {@link #calculateFareBikeWithLessThanThirtyMinutesParkingTime()} Unit Test
+	 * <br>
+	 * GIVEN: Parking exit time for the <b>BIKE</b> set to <b>less than 30
+	 * minutes</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare service checks for duration threshold below 30
+	 * minutes <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals fare = 0€
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals fare != 0€
+	 * <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareBikeWithLessThanThirtyMinutesParkingTime() {
 
@@ -161,6 +251,19 @@ public class FareCalculatorServiceTest {
 		assertEquals((0.0 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice());
 	}
 
+	/**
+	 * {@link #calculateFareCarWithLessThanThirtyMinutesParkingTime()} Unit Test
+	 * <br>
+	 * GIVEN: Parking exit time for the <b>CAR</b> set to <b>less than 30
+	 * minutes</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare service checks for duration threshold below 30
+	 * minutes <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals fare = 0€
+	 * <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals fare != 0€
+	 * <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareCarWithLessThanThirtyMinutesParkingTime() {
 
@@ -182,6 +285,18 @@ public class FareCalculatorServiceTest {
 		assertEquals((0.0 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
 	}
 
+	/**
+	 * {@link #calculateFareCarWithLessThanOneHourParkingTime()} Unit Test <br>
+	 * GIVEN: Parking exit time for the <b>BIKE</b> set to less than One hour and
+	 * more than 30 minutes i.e. <b>45 minutes</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare service checks for duration threshold below One
+	 * hour <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals fare = "0.75 *
+	 * Fare.CAR_RATE_PER_HOUR" <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals fare != "0.75 *
+	 * Fare.CAR_RATE_PER_HOUR" <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareBikeWithLessThanOneHourParkingTime() {
 
@@ -203,6 +318,18 @@ public class FareCalculatorServiceTest {
 		assertEquals((0.75 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice());
 	}
 
+	/**
+	 * {@link #calculateFareCarWithLessThanOneHourParkingTime()} Unit Test <br>
+	 * GIVEN: Parking exit time for the <b>CAR</b> set to less than One hour and
+	 * more than 30 minutes i.e. <b>45 minutes</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare service checks for duration threshold below One
+	 * hour <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals fare = "0.75 *
+	 * Fare.CAR_RATE_PER_HOUR" <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals fare != "0.75 *
+	 * Fare.CAR_RATE_PER_HOUR" <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareCarWithLessThanOneHourParkingTime() {
 
@@ -224,6 +351,18 @@ public class FareCalculatorServiceTest {
 		assertEquals((0.75 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
 	}
 
+	/**
+	 * {@link #calculateFareCarWithMoreThanADayParkingTime()} Unit Test <br>
+	 * GIVEN: Parking exit time for the <b>CAR</b> set to more than a day i.e. <b>24
+	 * hours</b> <br>
+	 * WHEN: calculation of fare service activated <br>
+	 * THEN: calculation of fare service checks for duration threshold for One day
+	 * and more <br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals (24 *
+	 * Fare.CAR_RATE_PER_HOUR) <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals (24 *
+	 * Fare.CAR_RATE_PER_HOUR) <code><b>FALSE</b></code>
+	 */
 	@Test
 	public void calculateFareCarWithMoreThanADayParkingTime() {
 
