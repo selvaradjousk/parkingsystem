b19ac47fb44feebcb6a86b7940316aaf4f3703fb
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-05-16 05:18:25 +0200

    [REFACTOR + TEST CASE] DataBaseConfigIT and Integration Test

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseConfigIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseConfigIT.java
@@ -1,40 +1,192 @@
 package com.parkit.parkingsystem.integration.config;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Assert;
-import org.junit.Before;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import com.parkit.parkingsystem.config.DataBaseConfig;
-import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 
+/**
+ * Class {@link DataBaseConfigIT} - Integration Testing For database connection
+ * setting configuration {@link DataBaseConfig}
+ * 
+ * @package - com.parkit.parkingsystem.integration.config
+ * @project - P4 - parking system - ParkIt
+ * @see Tests: {@link #testGetConnection()}, {@link #testCloseConnection()},
+ *      {@link #testClosePreparedStatement()}, {@link #testCloseResultSet()},
+ * 
+ * @author Senthil
+ */
 public class DataBaseConfigIT {
 
-	@InjectMocks
-	private DataBaseConfig dataBaseConfig;
-	@Mock
-	private Connection mockConnection;
-	@Mock
-	private Statement mockStatement;
-	
-	  @Before
-	  public void setUp() {
-		  MockitoAnnotations.openMocks(this);
-	  }
-	 
-	  @Test
-	  public void testMockDBConnection() throws Exception {
-	    Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
-	    Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(1);
-//	    int value = dataBaseConfig.executeQuery("");
-//	    Assert.assertEquals(value, 1);
-	    Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
-	  }
-	
+	static DataBaseConfig dataBaseConfig = new DataBaseConfig();
+
+	/**
+	 * Tests DataBaseConfig.getConnection
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * 
+	 * @see com.parkit.parkingsystem.config.DataBaseConfig#getConnection()
+	 */
+	@Test
+	/**
+	 * {@link #testGetConnection()} Integration Test on
+	 * {@link DataBaseConfig#getConnection()()}<br>
+	 * GIVEN: connection values set<br>
+	 * WHEN: executing get Connection <br>
+	 * THEN: expected Connection status <b>checked</b><br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals expected = testConnection.isClosed() <code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals expected != testConnection.isClosed() <code><b>FALSE</b></code>
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public void testGetConnection()
+			throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+		// GIVEN
+		String expectedDatabaseUrl = "jdbc:mysql://localhost:3306/prod";
+		boolean expectedConnectionClosed = false;
+
+		// WHEN
+		Connection testConnection = null;
+		testConnection = dataBaseConfig.getConnection();
+
+		// THEN
+		if (testConnection == null)
+			fail("Couldn't retrieve connection");
+
+		assertEquals(expectedConnectionClosed, testConnection.isClosed());
+
+		DatabaseMetaData metaData = testConnection.getMetaData();
+
+		assertEquals(expectedDatabaseUrl, metaData.getURL());
+		testConnection.close();
+	}
+
+
+	/**
+	 * {@link #testCloseConnection()} Integration Test on
+	 * {@link DataBaseConfig#closeConnection()()}<br>
+	 * GIVEN: connection values set<br>
+	 * WHEN: executing close Connection <br>
+	 * THEN: expected Connection status <b>checked</b><br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals expected = Connection
+	 * closed<code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals expected != Connection
+	 * closed<code><b>FALSE</b></code>
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	@Test
+	public void testCloseConnection()
+			throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+		// GIVEN
+		boolean expectedConnectionClosed = true;
+		Connection testConnection = null;
+
+		testConnection = dataBaseConfig.getConnection();
+
+		if (testConnection == null)
+			fail("Couldn't retrieve connection to start test");
+
+		// WHEN
+		dataBaseConfig.closeConnection(testConnection);
+
+		// THEN
+		assertEquals(expectedConnectionClosed, testConnection.isClosed());
+		testConnection.close();
+
+	}
+
+	/**
+	 * {@link #testClosePreparedStatement()} Integration Test on
+	 * {@link DataBaseConfig#closePreparedStatement()()}<br>
+	 * GIVEN: connection values set<br>
+	 * WHEN: executing close PreparedStatement <br>
+	 * THEN: expected PreparedStatement status <b>checked</b><br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals expected =
+	 * PreparedStatement closed<code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals expected !=
+	 * PreparedStatement closed<code><b>FALSE</b></code>
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	@Test
+
+	public void testClosePreparedStatement()
+			throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+		// GIVEN
+		Connection testConnection = null;
+		PreparedStatement testStatement = null;
+		boolean expectedStatementClosed = true;
+
+		testConnection = dataBaseConfig.getConnection();
+		testStatement = testConnection.prepareStatement("SELECT 5");
+
+		// WHEN
+		dataBaseConfig.closePreparedStatement(testStatement);
+
+		// THEN
+		assertEquals(expectedStatementClosed, testStatement.isClosed());
+		testConnection.close();
+		testStatement.close();
+
+	}
+
+	/**
+	 * {@link #testCloseResultSet()} Integration Test on
+	 * {@link DataBaseConfig#closeResultSet()()}<br>
+	 * GIVEN: connection values set<br>
+	 * WHEN: executing close ResultSet <br>
+	 * THEN: expected ResulSet status <b>checked</b><br>
+	 * <b>Test Condition <i>PASSED</i>: </b>assertEquals expected = resultset
+	 * closed<code><b>TRUE</b></code> <br>
+	 * <b>Test Condition <i>FAILED</i>: </b>assertEquals expected != resultset
+	 * closed<code><b>FALSE</b></code>
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	@Test
+	public void testCloseResultSet()
+			throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+		// GIVEN
+		Connection testConnection = null;
+		PreparedStatement testStatement = null;
+		ResultSet testResults = null;
+		boolean expectedResultSetClosed = true;
+
+		testConnection = dataBaseConfig.getConnection();
+		testStatement = testConnection.prepareStatement("Select 2");
+		testResults = testStatement.executeQuery();
+
+		// WHEN
+		dataBaseConfig.closeResultSet(testResults);
+
+		// THEN
+
+		assertEquals(expectedResultSetClosed, testResults.isClosed());
+
+		testConnection.close();
+		testStatement.close();
+		testResults.close();
+	}
 }
